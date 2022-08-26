ae116202ea50476869ff8a8ab6f7f656b6028406
t-riku <dingariku@icloud.com>
2022-08-26 10:49:06 +0900

    add hiroshiModal

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import hiroshi from "../assets/images/hiroshi.jpg";
 import houzz from "../assets/images/houzz.png";
@@ -7,8 +7,15 @@ import sumika from "../assets/images/SuMiKa.png";
 import aboutLinkPhilosophy from "../assets/images/about__link05.JPG";
 import aboutLinkFlow from "../assets/images/about__link01.JPG";
 import aboutLinkOverview from "../assets/images/about__link03.JPG";
+import HiroshiModal from "./Modal/HiroshiModal";
 
 const About = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const ShowModal = () => {
+    setShowModal(true);
+  };
+
   return (
     <>
       {/* about section */}
@@ -24,15 +31,11 @@ const About = () => {
             <div className="row g-3">
               <div className="member card-effect">
                 <div className="text-center member-card">
-                  <img
-                    src={hiroshi}
-                    alt="tada-hiroshi"
-                    data-micromodal-trigger="about-hiroshi"
-                  />
+                  <img src={hiroshi} alt="tada-hiroshi" onClick={ShowModal} />
                   <h5 className="mb-0 mt-4">多田 博</h5>
                   <p>一級建築士</p>
                   <a
-                    data-micromodal-trigger="about-hiroshi"
+                    onClick={ShowModal}
                     className="btn btn-outline-dark py-3 px-5 mt-4"
                   >
                     More
@@ -163,72 +166,14 @@ const About = () => {
             </div>
 
             <div className="row mt-5 button">
-              <Link to="#top" className="btn btn-outline-dark py-3">
+              <a href="#top" className="btn btn-outline-dark py-3">
                 TOPに戻る
-              </Link>
+              </a>
             </div>
           </section>
         </div>
-
-        {/* about-hiroshi modal */}
-        <div
-          className="modal micromodal-slide about-hiroshi"
-          id="about-hiroshi"
-          aria-hidden="true"
-        >
-          <div
-            className="modal__overlay"
-            //    tabindex="-1"
-            data-micromodal-close
-          >
-            <div
-              className="modal__container"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-1-title"
-            >
-              <header className="modal__header">
-                <div className="modal__header__box">
-                  <h2 className="modal__title" id="modal-1-title">
-                    多田 博
-                  </h2>
-                  <h5>一級建築士</h5>
-                </div>
-                <button
-                  className="modal__close"
-                  aria-label="Close modal"
-                  data-micromodal-close
-                ></button>
-              </header>
-              <main className="modal__content" id="modal-1-content">
-                <div className="container-fluid">
-                  <div className="row">
-                    <img src={hiroshi} />
-                  </div>
-                  <div className="row">
-                    <ul>
-                      <li className="modal__text">
-                        多田建築設計事務所主宰 一級建築士
-                        藤沢市都市計画審議会委員
-                        大学1年の時、建築家アントニオ・ガウディに魅せられ建築の素晴らしさを知る。関東学院大学工学部建築学科卒業後、㈱GS建築研究所入社。多くの住宅設計に携わる。29歳の時、建築を志すきっかけとなったガウディの作品を見るためスペインを旅する。特にサクラダファミリアの荘厳さに圧倒され、建築の奥深さを知る。その後、公共建築に興味を持ち㈱日総建に入社。保養施設のコンペに当選し設計・監理までチーフとして携わる。福祉施設、集合住宅、資料館、劇場、図書館、学校等あらゆる公共の施設を次々とチームリーダーとして手がける。富山県「八尾町おわら資料館」では「富山県建築賞」を受賞。2001年に「多田建築設計事務所」を共同主宰とする為独立。設立後は数多くの住宅、診療所、二世帯住宅を多く手がける。風土に根ざした建築を常に目指し、独立前の豊かな経験をフルに活かし設計・監理に努める。現在、藤沢市の都市計画審議会委員に選出され、地域の代表としてまちづくりに携わる。東京都大田区生まれ
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </main>
-              <footer className="modal__footer">
-                <button
-                  className="btn btn-outline-dark"
-                  data-micromodal-close
-                  aria-label="Close this dialog window"
-                >
-                  Close
-                </button>
-              </footer>
-            </div>
-          </div>
-        </div>
       </section>
+      <HiroshiModal showFlag={showModal} setShowModal={setShowModal} />
     </>
   );
 };
