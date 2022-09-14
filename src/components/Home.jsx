213f5687f46134a6042d988198d45d102f4ac16f
t-riku <dingariku@icloud.com>
2022-09-14 18:22:25 +0900

    change video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-// import bgHomeVideo from "../assets/images/Pexels Videos 2248564.mp4";
-import bgHomeVideo from "../assets/images/sampleTopVideo.mp4";
+import bgHomeVideo from "../assets/images/topVideo5.mp4";
 import houzzIcon from "../assets/images/houzz__icon.png";
 import suvacoIcon from "../assets/images/suvaco__icon.png";
 import sumikaIcon from "../assets/images/sumika__icon__rainbow.png";
@@ -11,16 +10,16 @@ const Home = () => {
       {/* toppage section */}
       <section className="toppage vh-100 w-100" id="top">
         {/* 重くなるから一旦コメントアウト。公開する時にはずす */}
-        {/* <video
+        <video
           className="videoVolume"
           src={bgHomeVideo}
           type="video/mp4"
           //   webkit-playsinline
-          //   playsinline
+          // playsinline
           autoPlay
           loop
           muted
-        /> */}
+        />
         <div className="overlay"></div>
         <div className="title">
           <h1>多田建築設計事務所</h1>
