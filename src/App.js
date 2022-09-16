c61bb09c62d3bcabe2457f90ff9007662799ef4e
t-riku <dingariku@icloud.com>
2022-09-16 14:55:01 +0900

    add loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import PuffLoader from "react-spinners/ClipLoader";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -12,36 +14,58 @@ import ScrollToTop from "./components/ScrollToTop";
 // import db from "./firebase";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <ScrollToTop />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Home />
-                  <Works />
-                  <About />
-                </>
-              }
+      {loading ? (
+        <section className="loading">
+          <div>
+            <PuffLoader
+              color={"#36d7b7"}
+              loading={loading}
+              size={50}
+              className="spinner"
             />
-            <Route path={`/philosophy`} element={<Philosophy />} />
-            <Route path={`/flow`} element={<Flow />} />
-            <Route path={`/overview`} element={<Overview />} />
-            <Route path={`/*`} element={<NotFound />} />
-            {/* <Route path={`/audio`} element={<Audio />} /> */}
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </BrowserRouter>
+          </div>
+          <p>loading...</p>
+        </section>
+      ) : (
+        <BrowserRouter>
+          <header>
+            <Header />
+          </header>
+          <main>
+            <ScrollToTop />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Home />
+                    <Works />
+                    <About />
+                  </>
+                }
+              />
+              <Route path={`/philosophy`} element={<Philosophy />} />
+              <Route path={`/flow`} element={<Flow />} />
+              <Route path={`/overview`} element={<Overview />} />
+              <Route path={`/*`} element={<NotFound />} />
+              {/* <Route path={`/audio`} element={<Audio />} /> */}
+            </Routes>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
