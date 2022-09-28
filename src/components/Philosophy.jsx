cdb9f671577ad741ed66a498d448b472cff995f2
t-riku <dingariku@icloud.com>
2022-09-28 17:02:24 +0900

    delete link py-5

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -217,7 +217,7 @@ const Philosophy = () => {
 
               {/* link ➡︎ 理念、事務所概要、設計の流れ */}
               <section className="link">
-                <div className="row g-5 link__wrapper py-5 d-flex justify-content-center">
+                <div className="row g-5 link__wrapper d-flex justify-content-center">
                   <div className="col-md-4">
                     <div className="link__item">
                       <img src={aboutLinkFlow} alt="aboutFlowImg" />
