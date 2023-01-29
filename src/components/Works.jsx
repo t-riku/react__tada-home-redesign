055d01f453dc676240a6b9a6298a6f2ec1c74172
t-riku <dingariku@icloud.com>
2023-01-29 15:14:12 +0900

    ✨ 管理ページで作品の順番を変更できるように

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import db from "../firebase";
-import { collection, getDocs, onSnapshot, doc } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import HouseModal from "./Modal/HouseModal";
 import ReactPlayer from "react-player";
 import bgHomeVideo from "../assets/images/全シーン完成.mp4";
 
 const Works = () => {
-  const [works, setWorks] = useState([]);
+  const [works, setWorks] = useState({});
+
+  const [order, setOrder] = useState({ house: [], design: [] });
 
   const [showModal, setShowModal] = useState(false);
 
@@ -25,12 +27,21 @@ const Works = () => {
     const workData = collection(db, "works");
     getDocs(workData).then((snapShot) => {
       // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
-      setWorks(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      const works = {};
+      snapShot.docs.forEach((doc) => {
+        const data = doc.data();
+        works[doc.id] = data;
+      });
+      setWorks(works);
     });
 
     // リアルタイムで取得
-    onSnapshot(workData, (work) => {
-      setWorks(work.docs.map((doc) => ({ ...doc.data() })));
+    // onSnapshot(workData, (work) => {
+    //   setWorks(work.docs.map((doc) => ({ ...doc.data() })));
+    // });
+
+    getDoc(doc(db, "orders/works")).then((snapShot) => {
+      setOrder(snapShot.data());
     });
   }, []);
   return (
@@ -49,17 +60,25 @@ const Works = () => {
           <section className="home row g-3 mt-1" id="home">
             <h4 className="text-center">~ 住宅 ~</h4>
             <div className="works__wrapper text-center">
-              {works.map((work, index) =>
-                work.type === "house" ? (
+              {order.house.map((id, index) =>
+                works[id] !== undefined ? (
                   <div className="works__item card-effect" key={index}>
-                    <a onClick={ShowModal(work)} className="works__img__a">
-                      <img src={work.thumbnailImageUrl} alt={work.caption} />
-                    </a>
-                    <h5 className="mb-1">{work.caption}</h5>
-                    <p>{work.title}</p>
                     <a
-                      onClick={ShowModal(work)}
+                      onClick={ShowModal(works[id])}
+                      className="works__img__a"
+                      href="#/"
+                    >
+                      <img
+                        src={works[id].thumbnailImageUrl}
+                        alt={works[id].caption}
+                      />
+                    </a>
+                    <h5 className="mb-1">{works[id].caption}</h5>
+                    <p>{works[id].title}</p>
+                    <a
+                      onClick={ShowModal(works[id])}
                       className="btn btn-outline-dark py-2 px-4 mt-1"
+                      href="#/"
                     >
                       More
                     </a>
@@ -89,16 +108,24 @@ const Works = () => {
           <section className="design row g-3 my-5" id="design">
             <h4 className="text-center">~ デザイン ~</h4>
             <div className="works__wrapper text-center">
-              {works.map((work, index) =>
-                work.type === "design" ? (
+              {order.design.map((id, index) =>
+                works[id] !== undefined ? (
                   <div className="works__item card-effect" key={index}>
-                    <a onClick={ShowModal(work)} className="works__img__a">
-                      <img src={work.thumbnailImageUrl} alt={work.caption} />
-                    </a>
-                    <h5 className="mb-0 mt-4">{work.title}</h5>
                     <a
-                      onClick={ShowModal(work)}
+                      onClick={ShowModal(works[id])}
+                      className="works__img__a"
+                      href="#/"
+                    >
+                      <img
+                        src={works[id].thumbnailImageUrl}
+                        alt={works[id].caption}
+                      />
+                    </a>
+                    <h5 className="mb-0 mt-4">{works[id].title}</h5>
+                    <a
+                      onClick={ShowModal(works[id])}
                       className="btn btn-outline-dark py-2 px-4 mt-1"
+                      href="#/"
                     >
                       More
                     </a>
